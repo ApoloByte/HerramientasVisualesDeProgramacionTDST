8b1eb4083df0e26ac31b3719c7ee995dfd8b6d9e
NOMBRE ESTUDIANTE <Apolo>
2025-09-09 17:52:19 -0500

    feat(guia-2): add pets exercise

--- 02-bases-react-guia-2/src/components/Pets.tsx
+++ 02-bases-react-guia-2/src/components/Pets.tsx
@@ -13,12 +13,17 @@ export const Pets = () => {
     return (
         <>
         <div>Pets</div>
+
+        <h2>Imprimir los elementos de una lista usando <b>map</b></h2>
         <div>
          {animales.map(
             (animal,index)=> <div key ={index}>
                 {animal}
             </div>
          )}
+
+        <h2>Imprimir los elementos de una lista separada por comas con <b>join</b></h2>
+         {animales.join(", ")}
 
         </div>
 
